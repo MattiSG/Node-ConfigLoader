221707b4373ea59f6625655eed2f0cb670308a23
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-07-20 11:55:42 +0200

    Started adding test cases.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,40 @@
 require('mocha');
-require('should');
+var assert = require('assert'),	// no point in using should, as config objects have no prototype, hence no “should” augmentation
+	pathUtils = require('path');
 
 var ConfigLoader = require('../src/ConfigLoader');
 
+var CONFIG_FILE = 'demo-config',
+	OUTER_FOLDER = pathUtils.join(__dirname, 'outer'),
+	MIDDLE_FOLDER = pathUtils.join(OUTER_FOLDER, 'middle'),
+	INNER_FOLDER = pathUtils.join(MIDDLE_FOLDER, 'inner');
 
-var loader = new ConfigLoader();
 
-var loaded = loader.load('demo-config');
+describe('Loading', function() {
+	
 
-console.error(loaded);
+	it('should not find anything from the package root', function() {
+		var loader = new ConfigLoader();
+
+		for (var k in loader.load(CONFIG_FILE))
+			assert.fail('Found "' + k + '" while no property should have been loaded!');
+	});
+
+	describe('when given the middle folder', function() {
+		var loader = new ConfigLoader({ from: MIDDLE_FOLDER });
+		var loaded = loader.load(CONFIG_FILE);
+
+		it('should find the middle value', function() {
+			assert.equal(loaded.from, 'middle');
+		});
+
+		it('should visit the outer directories', function() {
+			assert(loaded.middle);
+			assert(loaded.outer, 'Outer folder was not visited');
+		});
+
+		it('should not visit the inner directories', function() {
+			assert.equal(typeof loaded.inner, 'undefined' , 'Inner folder was visited');
+		});
+	})
+});
